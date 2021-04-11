d6a1f4b28fe846017e0017dd155b9582b165e912
Deadpool812 <chaitanya9joshi@gmail.com>
2021-04-11 16:20:23 +0530

    Fix for vercel

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== 'production';
 
 export const server = dev
   ? 'http://localhost:3000'
-  : `https://${process.env.VERCEL_URL}`;
+  : 'https://aquarius-gilt.vercel.app';
